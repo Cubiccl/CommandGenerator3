6434457c22052ad97c37e6d65f4afef1ead9d36a
Cubiccl <ccl.clement@hotmail.fr>
2017-12-03 13:14:36 +0100

    +previous

--- src/fr/cubiccl/generator3/CommandGenerator.java
+++ src/fr/cubiccl/generator3/CommandGenerator.java
@@ -1,15 +1,19 @@
 package fr.cubiccl.generator3;
 
-import fr.cubiccl.generator3.application.MainWindow;
 
 
 public class CommandGenerator
 {
 	
-	public static MainWindow window;
+	public static MainApplication window;
 	
 	public static void main(String[] args)
 	{
-		MainWindow.initialize(args);
+		MainApplication.initialize(args);
+	}
+
+	public static void exit()
+	{
+		window.primaryStage.close();
 	}
 }
